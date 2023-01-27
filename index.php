2450955edec67723bbeef315c542bdb54e730994
commando27 <56701905+commando27@users.noreply.github.com>
2023-01-27 13:56:21 -0500

    connexion, sinscrire
    mvp de la page conexion et s'inscrire et du index.php. Ajout de la bibliotheque bootstrap et de jquery

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenue sur notre de site de rencontre</h1>
+    <button onClick="location.href = 'sinscrire.php';">S'inscrire</button>
+    <button onClick="location.href = 'conexion.php';">Se connecter</button>
+</body>
+</html>
