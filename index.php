a6643945636729c4ac8c36da50004973deda016e
commando27 <56701905+commando27@users.noreply.github.com>
2023-01-27 19:27:47 -0500

    Ajout d'un logo temporaire
    Ajout d'un logo temporaire

--- index.php
+++ index.php
@@ -9,16 +9,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
 </head>
 <body>
     <!-- Image and text -->
     <nav class="navbar navbar-dark bg-primary">
         <a class="navbar-brand" href="#">
-            <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            <img src="public\images\logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
             LOML
         </a>
     </nav>
-    
+
     <h1>Bienvenue sur notre de site de rencontre</h1>
     <button onClick="location.href = 'sinscrire.php';">S'inscrire</button>
     <button onClick="location.href = 'conexion.php';">Se connecter</button>
