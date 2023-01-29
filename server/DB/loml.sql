ce3a277f98f09fb2311e677147721f1c559251db
commando27 <56701905+commando27@users.noreply.github.com>
2023-01-28 19:17:44 -0500

    Ajout de la base de donnée
    Ajout de la base de donnée

---
+++ server/DB/loml.sql
@@ -0,0 +1,278 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 28, 2023 at 10:55 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.1.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loml`
+--
+CREATE DATABASE IF NOT EXISTS `loml` DEFAULT CHARACTER SET utf16 COLLATE utf16_unicode_ci;
+USE `loml`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `connection`
+--
+
+DROP TABLE IF EXISTS `connection`;
+CREATE TABLE `connection` (
+  `idUser` int(11) NOT NULL,
+  `email` varchar(255) COLLATE utf16_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf16_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversation`
+--
+
+DROP TABLE IF EXISTS `conversation`;
+CREATE TABLE `conversation` (
+  `idConversation` int(11) NOT NULL,
+  `idUser1` int(11) NOT NULL,
+  `idUser2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE `likes` (
+  `idUser` int(11) NOT NULL,
+  `idLikedUser` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matchs`
+--
+
+DROP TABLE IF EXISTS `matchs`;
+CREATE TABLE `matchs` (
+  `idMatch` int(11) NOT NULL,
+  `idUser1` int(11) NOT NULL,
+  `idUser2` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages` (
+  `idMessages` int(11) NOT NULL,
+  `idConversation` int(11) NOT NULL,
+  `idSender` int(11) NOT NULL,
+  `idReceiver` int(11) NOT NULL,
+  `content` varchar(255) COLLATE utf16_unicode_ci NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+CREATE TABLE `profile` (
+  `idUser` int(11) NOT NULL,
+  `rank` int(11) NOT NULL,
+  `age` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `typeRelation` varchar(255) COLLATE utf16_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf16_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE `session` (
+  `idSession` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `idUser` int(11) NOT NULL,
+  `firstName` varchar(255) COLLATE utf16_unicode_ci NOT NULL,
+  `lastName` varchar(255) COLLATE utf16_unicode_ci NOT NULL,
+  `admin` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`idUser`, `firstName`, `lastName`, `admin`) VALUES
+(1, 'Hakam', 'Almotlak', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `connection`
+--
+ALTER TABLE `connection`
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Indexes for table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD PRIMARY KEY (`idConversation`),
+  ADD KEY `idUser1` (`idUser1`),
+  ADD KEY `idUser2` (`idUser2`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `idLikedUser` (`idLikedUser`);
+
+--
+-- Indexes for table `matchs`
+--
+ALTER TABLE `matchs`
+  ADD KEY `matchs_ibfk_1` (`idUser1`),
+  ADD KEY `matchs_idfk_2` (`idUser2`) USING BTREE;
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`idMessages`),
+  ADD KEY `idConversation` (`idConversation`),
+  ADD KEY `idReceiver` (`idReceiver`),
+  ADD KEY `idSender` (`idSender`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Indexes for table `session`
+--
+ALTER TABLE `session`
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `conversation`
+--
+ALTER TABLE `conversation`
+  MODIFY `idConversation` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `idMessages` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `connection`
+--
+ALTER TABLE `connection`
+  ADD CONSTRAINT `connection_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD CONSTRAINT `conversation_ibfk_1` FOREIGN KEY (`idUser1`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `conversation_ibfk_2` FOREIGN KEY (`idUser2`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`idLikedUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `matchs`
+--
+ALTER TABLE `matchs`
+  ADD CONSTRAINT `matchs_ibfk_1` FOREIGN KEY (`idUser1`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `matchs_ibfk_2` FOREIGN KEY (`idUser2`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`idConversation`) REFERENCES `conversation` (`idConversation`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`idReceiver`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_3` FOREIGN KEY (`idSender`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `session`
+--
+ALTER TABLE `session`
+  ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
