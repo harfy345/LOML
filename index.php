43b80780c29058af8d873f08aec9420297fdc2ef
commando27 <56701905+commando27@users.noreply.github.com>
2023-01-31 16:25:38 -0500

    maj index.php

--- index.php
+++ index.php
@@ -3,12 +3,26 @@ $pagetitre = "index";
 require_once("./public/util/header.php");
 
 ?>
+<link rel="stylesheet" href="./public/css/style.css">
+
+<div class="btn btn-primary col-6"  id="aaaaac">   
+
+    <div>
+        <h1 style = "color:blue;">Bienvenue sur notre de site de rencontre</h1>
+    </div>
+
+    <div class="row">
+        <h2> Love of my life </h2>
+        <div class="">   
+            <button class="btn btn-primary col-3" onClick="location.href = 'sinscrire.php';">S'inscrire</button>
+            <button class="btn btn-primary col-3" onClick="location.href = 'conexion.php';">Se connecter</button>
+        </div>
+    </div>
+
+ 
+</div>
 
 
-
-<h1>Bienvenue sur notre de site de rencontre</h1>
-<button onClick="location.href = 'sinscrire.php';">S'inscrire</button>
-<button onClick="location.href = 'conexion.php';">Se connecter</button>
 <?php
 
 require_once("./public/util/footer.php");
--- public/util/header.php
+++ public/util/header.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,11 +11,14 @@
 
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  
+
 </head>
 
 <body onLoad="initialiser(<?php echo "'" . $msg . "'" ?>);" class="d-flex flex-column min-vh-100" style='font-family: Arial, Helvetica, sans-serif;'> 
 <nav class="navbar navbar-dark navbar-expand-lg text-center sticky-top" style="background-color: blue;">
         <div id="navCont" class="container maxWidth">
+            <img src = "public/images/Logo.PNG" width ="60px" height ="60px" href="index.php">
             <a class="dr-title navbar-brand text-nav display-3" href="index.php">LOML</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <!-- <span class="navbar-toggler-icon"></span> -->
@@ -25,14 +27,16 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="navbar-nav me-auto">
                    
-                <a href="#" class="nav-item nav-link text-nav">link1</a>
-                <a href="#" class="nav-item nav-link text-nav">link2</a>
+                
 
                     
                 </div>
 
                 <div class="navbar-nav ms-auto">
-                    <a href="#" class="nav-item nav-link text-nav"> se connecter<i class="ms-1 fa-solid fa-user"></i></a>
+                    <a href="./conexion.php" class="nav-item nav-link text-nav"> se connecter<i class="ms-1 fa-solid fa-user"></i></a>
+                    <a href="./sinscrire.php" class="nav-item nav-link text-nav"> s'inscrire<i class="ms-1 fa-solid fa-user"></i></a>
+                    <a href="./about.php" class="nav-item nav-link text-nav">à propos</a>
+                    <a href="./contactUs.php" class="nav-item nav-link text-nav">contactez-nous</a>
                 </div>
 
                 
