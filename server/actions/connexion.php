e100a0a436768c723b39df267760d466b8c8e9b2
commando27 <56701905+commando27@users.noreply.github.com>
2023-01-31 00:16:41 -0500

    Update connexion.php

--- server/actions/connexion.php
+++ server/actions/connexion.php
@@ -1,45 +1,36 @@
 <?php
-
     require_once("../DB/databaseRequests.php");
     $courriel = $_POST['courrielc'];   
     $pass = $_POST['passc'];
 	$id=0;
 
-	$requete="SELECT * FROM connection  WHERE email=? and pass=?";
+	$requete="SELECT  users.idUser, connection.email, connection.pass, users.admin 
+	FROM users INNER JOIN connection ON users.idUser =connection.idUser WHERE connection.email = ? and connection.pass =?";
 	$stmt = $connexion->prepare($requete);
 	$stmt->bind_param("ss", $courriel , $pass);
 	$stmt->execute();
 	$result = $stmt->get_result();
 	if(!$ligne = $result->fetch_object()){
-        $msg = "Le courriel ou le mdp est incorrect";
+		// si on le trouve pas dans la bd
+		$msg = "Le courriel ou le mdp est incorrect";
         header("Location: ../../conexion.php?msg=$msg");
 		mysqli_close($connexion);
 		exit;
 	}else{
+		// si on le trouve
 		session_start();
 		$_SESSION['username'] =$courriel ;
-		
-		$id = $ligne->idUser;
-	}
-
-    $requete="SELECT * FROM users WHERE idUser=?";
-	$stmt = $connexion->prepare($requete);
-	$stmt->bind_param("i", $id);
-	$stmt->execute();
-	$result = $stmt->get_result();
-    $ligne=mysqli_fetch_object($result);
-
-		session_start();
-		$_SESSION['username'] =$courriel ;
-		$_SESSION['isAdmin'] =false ;
-	if($ligne->admin == 0){
-		
 		$msg = "le id est"."$id";
-
-		header("Location: ../../membres.php?msg=$msg");
-	} else{
-		
-		header("Location: ../../admin.php");
+		//si admin 
+		if($ligne->admin == 0){
+			$_SESSION['isAdmin'] =true ;
+			header("Location: ../../admin.php?msg=$msg");
+		}
+		// si membre
+		if(!$ligne->admin == 0){
+			$_SESSION['isAdmin'] =false ;
+			header("Location: ../../membres.php?msg=$msg");
+		}
 	}
 	
     mysqli_close($connexion);
