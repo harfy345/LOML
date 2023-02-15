c28d4bc7dcea0dedbcdc93f064e52f0ae09579fa
Newiii <119543655+Newiii@users.noreply.github.com>
2023-02-15 10:39:44 -0500

    update css page index

--- index.php
+++ index.php
@@ -16,15 +16,16 @@ require_once("./public/util/header.php");
 <body class="gb-index">
 
 
-<div class="d-flex justify-content-center my-auto w-100 h-100"> 
+<div class="d-flex justify-content-center my-auto w-100 h-100 div-inside-inscrire"> 
     
-    <section class="jumbotron text-center " id="aaaaac">
-        <div class="container">
+    <section class="text-center div-index-inside">
+        <div class="container div-index">
+          <img src="public\images\Logo-partern-white.png" alt="">
           <h1 class="jumbotron-heading">Bienvenue sur notre de site de rencontre</h1>
           <h2 class="lead text-muted">Love of my life</h2>
           <p>
-            <button class="btn btn-primary col-3" onClick="location.href = 'sinscrire.php';">S'inscrire</button>
-            <button class="btn btn-primary col-3" onClick="location.href = 'conexion.php';">Se connecter</button>
+            <button class="btn btn-dark col-3 btn-design-index" onClick="location.href = 'sinscrire.php';">S'inscrire</button>
+            <button class="btn btn-dark col-3 btn-design-index" onClick="location.href = 'conexion.php';">Se connecter</button>
           </p>
         </div>
       </section>
