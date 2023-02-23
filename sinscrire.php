a445ad2bd4309d9bfbb8bfb6c105e382fc60f5f7
Newiii <119543655+Newiii@users.noreply.github.com>
2023-02-23 10:22:07 -0500

    update css header / bootstrap page s'incrire et connexion

--- sinscrire.php
+++ sinscrire.php
@@ -26,7 +26,7 @@
             <div class="row justify-content-center ">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 ">
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">S'inscrire</p>
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 text-dark">S'inscrire</p>
 
                 <!-- Compléter l'action --- le lier au serveur -->
                 <form class="mx-1 mx-md-4" action="server/actions/enregistrerMembre.php" method="POST">
@@ -35,7 +35,7 @@
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="text" id="firstName" placeholder="Entrez votre prénom" name="firstName"  class="form-control" required/>
-                      <label class="form-label" for="form3Example1c">Votre prénom</label>
+                      <label class="form-label text-dark" for="form3Example1c">Votre prénom</label>
                     </div>
                   </div>
 
@@ -44,7 +44,7 @@
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="text" id="lastName" placeholder="Entrez votre nom" name="lastName"  class="form-control" required/>
-                      <label class="form-label" for="form3Example1c">Votre nom</label>
+                      <label class="form-label text-dark" for="form3Example1c">Votre nom</label>
                     </div>
                   </div>
 
@@ -52,7 +52,7 @@
                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="email" id="email" placeholder="toto@exemple.com" name="email"class="form-control" required/>
-                      <label class="form-label" for="form3Example3c">Votre courriel</label>
+                      <label class="form-label text-dark" for="form3Example3c">Votre courriel</label>
                     </div>
                   </div>
 
@@ -60,7 +60,7 @@
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="number" id="age" placeholder="Entrez votre âge" min="18" max="120" name="age" class="form-control" required/>
-                      <label class="form-label" for="form3Example4c">Votre âge</label>
+                      <label class="form-label text-dark" for="form3Example4c">Votre âge</label>
                     </div>
                   </div>
 
@@ -68,7 +68,7 @@
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="password"  id="pass" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Le mot de passe doit contenir au moins un chiffre, une lettre majuscule et une lettre minuscule, et avoir au moins 8 caractères ou plus " name="pass" class="form-control" required/>
-                      <label class="form-label" for="form3Example4c">Mot de passe</label>
+                      <label class="form-label text-dark" for="form3Example4c">Mot de passe</label>
                     </div>
                   </div>
 
@@ -76,7 +76,7 @@
                     <i class="fas fa-key fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="password" id="pass"  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Le mot de passe doit contenir au moins un chiffre, une lettre majuscule et une lettre minuscule, et avoir au moins 8 caractères ou plus " name="pass2" class="form-control" required/>
-                      <label class="form-label" for="form3Example4cd">Confirmer votre mot de passe</label>
+                      <label class="form-label text-dark" for="form3Example4cd">Confirmer votre mot de passe</label>
                     </div>
                   </div>
 
@@ -84,7 +84,7 @@
                     <input class="form-check-input check-box me-2" type="checkbox" value="" id="conditionCheckBox" required
                    
                     />
-                    <label class="form-check-label" for="form2Example3">
+                    <label class="form-check-label text-dark" for="form2Example3">
                     J'accepte toutes les déclarations dans les <a href="#!" class="link-dark"> Conditions d'utilisation</a>
                     </label>
                   </div>
