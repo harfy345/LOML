0de859cae96b128edd8933d0560a98d5cffd93c5
Ricardo Jean <61395065+Ricardoj-tech@users.noreply.github.com>
2023-03-07 15:05:31 -0500

    Update footer.php
    Footer de notre site

--- public/util/footer.php
+++ public/util/footer.php
@@ -1,13 +1,115 @@
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css " rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 </main>
-<footer>
-<div>copy right</div>
-</footer>
+<footer class="bg-dark text-center text-lg-start text-white" style="width:100%;">
+
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-6 border-bottom" >
+  
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4 link-secondary">
+            <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+            <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+            <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+            <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+            <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 link-secondary">
+            <i class="fab fa-github"></i>
+            </a>
+        </div>
+    <!-- Right -->
+     </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+     <section class="">
+     <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+        <div class="row mt-3">
+        <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+             <i class="fas fa-gem me-3 text-secondary"></i>LOML
+            </h6>
+            <p>
+            Des relations saines sont essentielles pour vivre une vie positive et productive.
+            Nous constituons un réseau social qui vous permet de vous sentir autonome tout en établissant ces relations.
+             </p>
+            </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                 Langages utilisés
+            </h6>
+                <p>
+                    <i href="#!" class="text-reset">Javascript</i>
+                </p>
+                <p>
+                    <i href="#!" class="text-reset">PHP</i>
+                </p>
+                <p>
+                    <i href="#!" class="text-reset">Bootstrap</i>
+                </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+           Contact
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">info@loml.com</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">(514)-012-4568</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">(438)-012-3456</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Adresse</h6>
+          <p><i class="fas fa-home me-3 text-secondary"></i> 9155 Rue Saint-Hubert</p>
+          <p><i class="fas fa-home me-3 text-secondary"></i> Montréal, QC </p>
+          <p><i class="fas fa-home me-3 text-secondary"></i> H2M 1Y7</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-2">
+    © 2023 LOML:
+    <a class="text-reset fw-bold" href="#">Tous droits réservés.</a>
+  </div>
+  <!-- Copyright -->
 </body>
 </html>
+
+
+
+
