011d236a863389e883dbde08e29b924a062b7eb9
harfy345 <hakam.almotlk345@gmail.com>
2023-03-09 10:09:18 -0500

    Update sendRecoveryMail.php

--- server/actions/sendRecoveryMail.php
+++ server/actions/sendRecoveryMail.php
@@ -1,5 +1,4 @@
 <?php
-declare(strict_types=1);
 
 require_once("./../apis/membre.php");
 $email = $_POST['courrielc'];
@@ -42,44 +41,37 @@ echo "<a href='../../passwordRecovery.php?id=$encryption'>click</a>";
 // mail('hakam.almotlk345@gmail.com', 'My Subject', $encryption , $headers);
 
 
-require 'mail/vendor/autoload.php'; 
-require_once("mail/config.php");
 
-use \SendGrid\Mail\Mail;
+?>
 
-$email = new Mail();
-// Replace the email address and name with your verified sender
-$email->setFrom(
-    '201911524@collegeahuntsic.qc.ca',
-    'Example Recipient'
-);
-$email->setSubject('Sending with Twilio SendGrid is Fun');
-// Replace the email address and name with your recipient
-$email->addTo(
-    // remplacer le email du client
-    $email,
-    'Example Sender'
-);
-// le html que vous voulez envoyer
-$email->addContent(
-    'text/html',
-    `<strong>localhost:80/passwordRecovery.php?id=$encryption.</strong>`
-);
 
+<?php
+require '../../mail/vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("<PATH TO>/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+require_once("../../mail/config.php");
+
+
+$mailer = new \SendGrid\Mail\Mail(); 
+$mailer->setFrom("201911524@collegeahuntsic.qc.ca", "Example User");
+$mailer->setSubject("Sending with SendGrid is Fun");
+$mailer->addTo($email, "Example User");
+$mailer->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$mailer->addContent(
+    "text/html", "<strong>localhost:80/passwordRecovery.php?id=$encryption</strong>"
+);
 $sendgrid = new \SendGrid(sendgrid_api);
 try {
-    $response = $sendgrid->send($email);
-    printf("Response status: %d\n\n", $response->statusCode());
-
-    $headers = array_filter($response->headers());
-    echo "Response Headers\n\n";
-    foreach ($headers as $header) {
-        echo '- ' . $header . "\n";
-    }
-
-    // rediriger vers la bonne page 
+    $response = $sendgrid->send($mailer);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
-        // affichier un message d'erreur
 }
 ?>
