49aba9d85042e3a076f8ef0743eea0a23747ba67
Christopher <totof2724@hotmail.com>
2023-03-16 21:27:27 -0400

    Update mail.php

--- server/actions/mail.php
+++ server/actions/mail.php
@@ -1,36 +1,66 @@
 <?php
 
 require_once("../DB/databaseRequests.php");
-
-$VotreAdresseMail="logic.newii@gmail.com";
-if(empty($_POST['email'])) {
-    echo "Le champ mail est vide";
-} else {
-        if(!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i",$_POST['email'])){
-            echo "L'adresse mail entrée est incorrecte";
-        }else{
-            if(empty($_POST['subject'])) {
-                echo "Le champ sujet est vide";
-            }else{
-                if(empty($_POST['message'])) {
-                    echo "Le champ message est vide";
-                }else{
-                    $Entetes = "MIME-Version: 1.0\r\n";
-                    $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
-                    $Entetes .= "From: LOML <".$_POST['email'].">\r\n";
-                    $Entetes .= "Reply-To: LOML <".$_POST['email'].">\r\n";
-                    $Mail=$_POST['email']; 
-                    $Sujet='=?UTF-8?B?'.base64_encode($_POST['subject']).'?=';
-                    $Message=htmlentities($_POST['message'],ENT_QUOTES,"UTF-8");
-                if(mail($VotreAdresseMail,$Sujet,nl2br($Message),$Entetes)){
-                        echo "L'émail à été envoyé avec succès!";
-                        header("./contactUs.php");
-                } else {
-                    echo "Une erreur est survenue, le mail n'a pas été envoyé";
-                    header("./contactUs.php");
-                }
-            }
-        }
+  if (isset($_POST['name']))
+    $name = $_POST['name'];
+  if (isset($_POST['email']))
+    $email = $_POST['email'];
+  if (isset($_POST['message']))
+    $message = $_POST['message'];
+  if (isset($_POST['subject']))
+    $subject = $_POST['subject'];
+  if ($name === '') {
+    echo "Name cannot be empty.";
+    die();
+  }
+  if ($email === '') {
+    echo "Email cannot be empty.";
+    die();
+  } else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo "Email format invalid.";
+      die();
     }
+  }
+  if ($subject === '') {
+    echo "Subject cannot be empty.";
+    die();
+  }
+  if ($message === '') {
+    echo "Message cannot be empty.";
+    die();
+  }
+
+ 
+
+
+require '../../mail/vendor/autoload.php'; 
+require_once("../../mail/config.php");
+
+$mailer = new \SendGrid\Mail\Mail(); 
+$mailer->setFrom("201911524@collegeahuntsic.qc.ca", "Example User");
+$mailer->setSubject($subject ? $subject : "unknown");
+$mailer->addTo("hakam.almotlk345@gmail.com", "Example User");
+$mailer->addTo("totof2724@hotmail.com", "Example User");
+$mailer->addContent(
+    "text/html",
+    "<strong>$email</strong><br/><strong>$message</strong>"
+);
+
+$sendgrid = new \SendGrid(sendgrid_api);
+try {
+    $response = $sendgrid->send($mailer);
+    printf("Response status: %d\n\n", $response->statusCode());
+
+    $headers = array_filter($response->headers());
+    echo "Response Headers\n\n";
+    foreach ($headers as $header) {
+        echo '- ' . $header . "\n";
+    }
+    $msg = "l'email a été envoyé.";
+    header("Location: ../../index.php?msg=$msg");
+  echo $msg;
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
 }
 ?>
